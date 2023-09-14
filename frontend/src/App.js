64b77d551946915ebb0071aac4fc640026f3afa0
bhuwan-darai <dr.darai29@gmail.com>
2023-09-14 23:39:54 +0545

    created a header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { Container } from "react-bootstrap";
+import Headers from "./components/Headers";
+import Footer from "./components/Footer";
 const App = () => {
-  return <p>this is pargraph </p>;
+  return (
+    <>
+      <Headers />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
